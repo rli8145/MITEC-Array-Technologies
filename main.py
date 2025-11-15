2f13eb63cb195d799d44a0081960f3d8de19d171
rli8145 <rli8145@gmail.com>
2025-11-15 12:36:14 -0500

    Integrated cities and ports data

--- main.py
+++ main.py
@@ -5,14 +5,40 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-steel_types = []
 df = pd.read_csv("")
 
-dest = {}
+dest = {
+    "Los Angeles": ["Los Angeles", 0],
+    "Chicago": ["Chicago", 0],
+    "New York": ["New York & New Jersey", 0],
+    "Dallas": ["Houston", 239], 
+    "Houston": ["Houston", 0] ,  
+    "Detroit": ["Detroit",0] ,  
+    "Minneapolis": ["Duluth", 154], 
+    "Boston": ["Boston", 0],
+    "Seattle": ["Seattle", 0],
+    "Philadelphia": ["Philadelphia", 0],
+    "San Jose": ["Oakland", 41],  
+    "Atlanta": ["Savannah", 248],  
+    "San Francisco": ["Oakland", 0],
+    "Cleveland": ["Cleveland", 0],  
+    "Portland": ["Portland", 0],
+    "Milwaukee": ["Milwaukee", 0],   
+    "Phoenix": ["Los Angeles", 372],  
+    "St. Louis": ["St. Louis", 0],  
+    "Cincinnati": ["Cincinnati", 0],  
+    "San Diego": ["San Diego", 0],
+    "Grand Rapids": ["Detroit", 157], 
+    "Indianapolis": ["New York & New Jersey", 708],  
+    "Pittsburgh": ["Pittsburgh", 0],    
+    "Kansas City": ["Kansas City", 0],   
+    "Memphis": ["Memphis", 0], 
+    "Albuquerque": ["Los Angeles", 787] # example for Array Technologies
+}
+
 dest.df = pd.DataFrame(dest)
 
-source = {}
-source.df = pd.DataFrame(source)
+
 
 # return satisfactory cities given constraints
 def satisfy(CO2_cap, price_cap, time_cap, steel_type):
