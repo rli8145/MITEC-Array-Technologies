cb378c834fe5ad0063ae67e2cef84599c676415e
Edwin Lin <burgerlivesmatter@gmail.com>
2025-11-15 20:15:54 -0500

    charts and card only 6 files

---
+++ frontend/app/components/Card.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+import OverallCostPie from "./OverallCostPie";
+import SourcesofCarbon from "./SourcesofCarbon";
+import { useSearchParams } from "next/navigation";
+import { motion } from "framer-motion";
+
+const Card = () => {
+  const co2 = "co2";
+  const methane = "methane";
+  const time = "tikme";
+  const cost = "cost";
+
+  const route = "Route";
+
+  const searchParams = useSearchParams();
+  const isClicked = searchParams.get("query") === "clicked";
+  return (
+    <div>
+      {isClicked && (
+        <motion.div
+          initial={{ opacity: 0, y: 15 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <div className="border-1 h-[%100] pt-15 pb-250 relative">
+            <div className="top-6 left-6 absolute text-3xl ">
+              <h1> Recommended Route: {route} </h1>
+            </div>
+            <hr className="mt-5" />
+
+            <div className="h-2/4 w-full absolute  flex  justify-between p-4">
+              <div className="left-side flex flex-col justify-start pt-15 space-y-10 font-bold text-2xl px-5 mt-1">
+                <div className="flex flex-col space-y-1">
+                  <span className="text-2xl font-bold">CO2 Pollution</span>
+                  <span className="text-base text-gray-600 font-medium">
+                    {co2}
+                  </span>
+                </div>
+
+                <div className="flex flex-col space-y-1">
+                  <span className="text-2xl font-bold">Amount of Methane</span>
+                  <span className="text-base text-gray-600 font-medium">
+                    {methane}
+                  </span>
+                </div>
+
+                <div className="flex flex-col space-y-1">
+                  <span className="text-2xl font-bold">Time to location</span>
+                  <span className="text-base text-gray-600 font-medium">
+                    {time}
+                  </span>
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <span className="text-2xl font-bold">Total Cost ($)</span>
+                  <span className="text-base text-gray-600 font-medium">
+                    {cost}
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-15">
+                <div className="flex px-25">
+                  <OverallCostPie />
+                  <div></div>
+                  <SourcesofCarbon />
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,19 +3,19 @@ import Sidemenu from "../components/sidemenu";
 import Input from "../components/Input";
 import Greetings from "../components/Greetings";
 import Map from "../components/Map";
-
+import Card from "../components/Card";
 export default function Home() {
   return (
-    <div className='flex gap-10 py-15 px-7 bg-base-100'>
+    <div className="flex gap-10 py-15 px-7 bg-base-100">
       <Sidemenu />
-      <div className='flex flex-col gap-2 flex-1'>
+      <div className="flex flex-col gap-2 flex-1">
         <Greetings />
-        <div className='flex flex-row gap-2'>
-        <Input />
-        <Map />
-
+        <div className="flex flex-row gap-2">
+          <Input />
+          <Map />
         </div>
 
+        <Card/>
       </div>
     </div>
   );
